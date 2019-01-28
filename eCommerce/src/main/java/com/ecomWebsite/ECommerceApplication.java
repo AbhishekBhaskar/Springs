093ee9e3f083a35c16f25c8cf79b8f219df9f892
Abhishek <abhishekbhaskar27@gmail.com>
2019-01-28 17:26:38 +0530

    Add files via upload

---
+++ eCommerce/src/main/java/com/ecomWebsite/ECommerceApplication.java
@@ -0,0 +1,16 @@
+package com.ecomWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.ecomWebsite.controller")
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
+
---
+++ eCommerce/src/main/java/com/ecomWebsite/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ecomWebsite.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ecomWebsite.modal.organization;;
+public interface UserRepository extends CrudRepository<organization,Integer>{
+
+}
