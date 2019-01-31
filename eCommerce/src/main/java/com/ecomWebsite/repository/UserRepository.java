36d407a99b1219a0efc5d55ed9f4a475eb386950
Abhishek <abhishekbhaskar27@gmail.com>
2019-01-31 16:47:03 +0530

    Delete UserRepository.java

--- eCommerce/src/main/java/com/ecomWebsite/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecomWebsite.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ecomWebsite.modal.organization;;
-public interface UserRepository extends CrudRepository<organization,Integer>{
-
-}
