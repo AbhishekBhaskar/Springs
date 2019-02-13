972eaadec559abfe822d6edd54b05b27182e2e42
Abhishek <abhishekbhaskar27@gmail.com>
2019-02-13 17:20:05 +0530

    Add files via upload

---
+++ automateCG/src/main/resources/static/disp_header.js
@@ -0,0 +1,11 @@
+function myfn()
+	{
+		var id=document.getElementById("click");
+		id.style.display="block";
+	/*	var org=document.getElementById("organizations");
+		
+		org.style.display="block";
+		alert("Clicked element is "+id);*/
+	
+	
+	}	
