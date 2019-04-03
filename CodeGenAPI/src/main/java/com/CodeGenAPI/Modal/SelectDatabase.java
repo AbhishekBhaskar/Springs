02c8e913a71fa43effb8de1a37a6e169338072f6
Abhishek <abhishekbhaskar27@gmail.com>
2019-04-03 17:21:28 +0530

    Add files via upload

---
+++ CodeGenAPI/src/main/java/com/CodeGenAPI/Modal/SelectDatabase.java
@@ -0,0 +1,22 @@
+package com.CodeGenAPI.Modal;
+
+public class SelectDatabase {
+	String selectedDB;
+	public SelectDatabase()
+	{
+		
+	}
+	public SelectDatabase(String selectedDB) {
+		super();
+		this.selectedDB = selectedDB;
+	}
+	public String getSelectedDB() {
+		return selectedDB;
+	}
+	public void setSelectedDB(String selectedDB) {
+		this.selectedDB = selectedDB;
+	}
+	
+	
+
+}
